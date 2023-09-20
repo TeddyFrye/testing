28008ceae7dadd565e6d6bd2baae629f7ba61b41
TeddyFrye <tfrye1227@gmail.com>
2023-09-20 14:25:19 -0500

    Adds .json files and working test file

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-// Test for capitalize function
-// Lowercase word
-test("capitalizes first leter of string", () => {
-  expect(capitalize("hello")).toBe("Hello");
-});
-
-// Empty string
-test("returns empty string unchanged", () => {
-  expect(capitalize("")).toBe("");
-});
-
-// Already capitalized
-test("returns already capitalized string unchanged", () => {
-  expect(capitalize("Hello")).toBe("Hello");
-});
-
-// String that starts with a non-alphabet character
-test("returns string with non-alphabetic start unchanged", () => {
-  expect(capitalize("123hello")).toBe("123hello");
-});
-
 //Actual Function
 function capitalize(inputStr) {
   if (!inputStr) return "";
